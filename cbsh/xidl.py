9d12f6a884e2ef97520942f0b929490ca51d9c8c
Tobias Oberstein <tobias.oberstein@crossbario.com>
2018-04-23 20:29:21 +0200

    extract and resolve referenced types

--- cbsh/xidl.py
+++ cbsh/xidl.py
@@ -93,6 +93,30 @@ schema_by_uri = {
 #                   => ParseDecl()
 #
 
+EXTRACT_ATTRS_RAW = False
+
+def extract_attributes(item, allowed_attributes=None):
+    num_attrs = item.AttributesLength()
+    attrs = [item.Attributes(i) for i in range(num_attrs)]
+    attrs_dict = {
+        x.Key().decode('utf8'): x.Value().decode('utf8') if x.Value().decode('utf8') not in ['0'] else None
+        for x in attrs
+    }
+    if allowed_attributes:
+        for attr in attrs_dict:
+            if attr not in allowed_attributes:
+                raise Exception('invalid XBR attribute  "{}" - must be one of {}'.format(attr, allowed_attributes))
+    return attrs_dict
+
+def extract_docs(item):
+    num_docs = item.DocumentationLength()
+    item_docs = [
+        item.Documentation(i).decode('utf8').strip()
+        for i in range(num_docs)
+    ]
+    return item_docs
+    
+
 filepath = os.path.abspath(filename)
 
 with open(filepath, 'rb') as f:
@@ -116,157 +140,221 @@ with open(filepath, 'rb') as f:
     schema['meta'] = schema_meta
     schema_by_uri['meta'] = schema_meta
 
-    # iterate over enums
+    enums = []
+    objects = []
+    services = []
+
+    fqn2type = {}
+
+    enum_cnt = 0
+    object_cnt = 0
+    service_cnt = 0
+
+    for i in range(_schema.EnumsLength()):
+        item = _schema.Enums(i)
+        name = item.Name().decode('utf8')
+        if name in fqn2type:
+            raise Exception('duplicate name '.format(name))
+        enum_cnt += 1
+
+    for i in range(_schema.ObjectsLength()):
+        item = _schema.Objects(i)
+        name = item.Name().decode('utf8')
+        if name in fqn2type:
+            raise Exception('duplicate name '.format(name))
+        object_cnt += 1
+
+    for i in range(_schema.ServicesLength()):
+        item = _schema.Services(i)
+        name = item.Name().decode('utf8')
+        if name in fqn2type:
+            raise Exception('duplicate name '.format(name))
+        service_cnt += 1
+
+    print('Processing schema with {} enums, {} objects and {} services'.format(enum_cnt, object_cnt, service_cnt))
+
+
+    # enums
     #
     num_enums = _schema.EnumsLength()
-    enums = []
     for i in range(num_enums):
+
+        # extract enum base information
+        #
         _enum = _schema.Enums(i)
         enum_name = _enum.Name().decode('utf8')
+        print('Processing enum {} ("{}")'.format(i, enum_name))
 
         enum = {
+            '_index': i,
             'type': 'enum',
             'name': enum_name,
+            'docs': extract_docs(_enum),
         }
+        if EXTRACT_ATTRS_RAW:
+            enum['attr'] = extract_attributes(_enum)
 
-        num_enum_values = _enum.ValuesLength()
-        enum_values = []
-        for j in range(num_enum_values):
-            _enum_value = _enum.Values(j)
-
-            enum_value = {
-                'name': _enum_value.Name().decode('utf8'),
-            }
-
-            num_enum_value_docs = _enum_value.DocumentationLength()
-            enum_value_docs = [
-                _enum_value.Documentation(i).decode('utf8').strip()
-                for i in range(num_enum_value_docs)
-            ]
-            enum_value['docs'] = enum_value_docs
-
-            enum_values.append(enum_value)
-
-        enum_values = sorted(enum_values, key=lambda enum_value: enum_value['name'])
+        # extract enum values
+        #
         enum_values_dict =  {}
-        for e in enum_values:
-            enum_values_dict[e['name']] = {
-                'docs': e['docs']
+        for j in range(_enum.ValuesLength()):
+            _enum_value = _enum.Values(j)
+            enum_value_name = _enum_value.Name().decode('utf8')
+            enum_value = {
+                'docs': extract_docs(_enum_value),
+                # enum values cannot have attributes
             }
+            if enum_value_name in enum_values_dict:
+                raise Exception('duplicate enum value "{}"'.format(enum_value_name))
+            enum_values_dict[enum_value_name] = enum_value
         enum['values'] = enum_values_dict
-
-        num_enum_docs = _enum.DocumentationLength()
-        enum_docs = [
-            _enum.Documentation(i).decode('utf8').strip()
-            for i in range(num_enum_docs)
-        ]
-        enum['docs'] = enum_docs
-
-        enums.append(enum)
 
         if enum_name in schema_by_uri['uri']:
             raise Exception(
                 'unexpected duplicate definition for qualified name "{}"'.
                 format(enum_name))
-        else:
-            schema_by_uri['uri'][enum_name] = enum
 
-    # iterate over objects (framebuffer structs)
+        enums.append(enum)
+        schema_by_uri['uri'][enum_name] = enum
+
+    # objects (tables/structs)
     #
-    num_objs = _schema.ObjectsLength()
-    objects = []
-    for i in range(num_objs):
+    for i in range(_schema.ObjectsLength()):
+
         _obj = _schema.Objects(i)
         obj_name = _obj.Name().decode('utf8')
-
-        # skip our "void marker"
-        if obj_name in ['Void']:
-            continue
+        print('Processing object {} ("{}")'.format(i, obj_name))
 
         obj = {
+            '_index': i,
             'type': 'table',
             'name': obj_name,
+            'docs': extract_docs(_obj),
         }
+        print('OBJECT                     ', i, obj_name)
+        if EXTRACT_ATTRS_RAW:
+            obj['attr'] = extract_attributes(_obj)
 
+        # extract fields
         num_fields = _obj.FieldsLength()
         fields = []
         fields_by_name = {}
         for j in range(num_fields):
+
             _field = _obj.Fields(j)
+            field_name = _field.Name().decode('utf8')
+            print('Processing field {} ("{}")'.format(i, field_name))
+
             _field_type = _field.Type()
             _field_index = int(_field_type.Index())
-            _field_name = _field.Name().decode('utf8')
             _field_base_type = _BASETYPE_ID2NAME.get(_field_type.BaseType(), None)
             _field_element = _BASETYPE_ID2NAME.get(_field_type.Element(), None)
             if _field_element == 'None':
                 _field_element = None
 
             field = {
+                '_index': j,
                 #'def': 'field',
-                #'name': _field_name,
+                #'name': field_name,
                 'id': int(_field.Id()),
                 'offset': int(_field.Offset()),
                 'base_type': _field_base_type,
             }
             if _field_element:
-                field['field_element'] = _field_element
+                field['element_type'] = _field_element
+
             if _field_index != -1:
-                field['field_index'] = _field_index
+
+                #field['field_index'] = _field_index
+
+                if _field_base_type == 'Obj' or _field_element == 'Obj':
+
+                    # obj/struct
+                    if _field_index < _schema.ObjectsLength():
+                        _obj_ref = _schema.Objects(_field_index).Name().decode('utf8')
+                        print('Found referenced table/struct: {}'.format(_obj_ref))
+                        field['referenced_type'] = _obj_ref
+                    else:
+                        print('WARNING - referenced table/struct for index {} ("{}.{}") not found'.format(_field_index, obj_name, field_name))
+                        field['referenced_type'] = '**********************'
+
+                elif _field_base_type in ['UType' ,
+                                          'Bool',
+                                          'Byte',
+                                          'UByte',
+                                          'Short',
+                                          'UShort',
+                                          'Int',
+                                          'UInt',
+                                          'Long',
+                                          'ULong',
+                                          'Float',
+                                          'Double',
+                                          'String']:
+                    # enum
+                    if _field_index < len(enums):
+                        _enum_ref = enums[_field_index]['name']
+                        print('Found referenced enum: {}'.format(_enum_ref))
+                        field['referenced_type'] = _enum_ref
+                    else:
+                        print('WARNING - referenced enum not found')
+                        field['referenced_type'] = '**********************'
+
+                else:
+                    raise Exception('unhandled field type: {} {} {} {}'.format(field_name, _field_base_type, _field_element, _field_index))
+
+            field_docs = extract_docs(_field)
+            if field_docs:
+                field['docs'] = field_docs
+
+            _field_attrs = extract_attributes(_field)
+            if _field_attrs:
+                field['attr'] = _field_attrs
 
             fields.append(field)
-            fields_by_name[_field_name] = field
+            fields_by_name[field_name] = field
 
         obj['fields'] = fields_by_name
-
-        num_obj_docs = _obj.DocumentationLength()
-        obj_docs = [
-            _obj.Documentation(i).decode('utf8').strip()
-            for i in range(num_obj_docs)
-        ]
-        obj['docs'] = obj_docs
-
-        objects.append(obj)
 
         if obj['name'] in schema_by_uri['uri']:
             raise Exception(
                 'unexpected duplicate definition for qualified name "{}"'.
                 format(field['name']))
+
+        # always append the object here, so we can dereference indexes correctly
+        objects.append(obj)
+
+        # skip our "void marker"
+        if False and obj_name in ['Void']:
+            pass
         else:
             schema_by_uri['uri'][obj['name']] = obj
 
     # iterate over services
     #
     num_services = _schema.ServicesLength()
-    services = []
     for i in range(num_services):
         _service = _schema.Services(i)
 
-        num_service_attrs = _service.AttributesLength()
-        service_attrs = [
-            _service.Attributes(i) for i in range(num_service_attrs)
-        ]
-        service_attrs_dict = {
-            x.Key().decode('utf8'): x.Value().decode('utf8')
-            for x in service_attrs
-        }
+        service_name = _service.Name().decode('utf8')
 
-        for attr in service_attrs_dict:
-            if attr not in INTERFACE_ATTRS:
-                raise Exception('invalid XBR interface attribute "{}" - must be one of {}'.format(attr, INTERFACE_ATTRS))
+        service_attrs_dict = extract_attributes(_service, INTERFACE_ATTRS)
 
         service_type = service_attrs_dict.get('type', None)
         if service_type != 'interface':
             raise Exception('invalid value "{}" for attribute "type" in XBR interface'.format(service_type))
 
-        service_uuid = service_attrs_dict.get('uuid', None)
-
-        service_name = _service.Name().decode('utf8')
         service = {
+            '_index': i,
             'type': service_type,
             'name': service_name,
-            'uuid': service_uuid,
+            'docs': extract_docs(_service),
         }
-        #service['attrs'] = service_attrs_dict
+
+        if EXTRACT_ATTRS_RAW:
+            service['attrs'] = service_attrs_dict
+        else:
+            service['uuid'] = service_attrs_dict.get('uuid', None)
 
         num_calls = _service.CallsLength()
         calls = []
@@ -276,18 +364,7 @@ with open(filepath, 'rb') as f:
 
             _call_name = _call.Name().decode('utf8')
 
-            num_call_attrs = _call.AttributesLength()
-            call_attrs = [_call.Attributes(i) for i in range(num_call_attrs)]
-            call_attrs_dict = {
-                x.Key().decode('utf8'): x.Value().decode('utf8')
-                for x in call_attrs
-            }
-
-            num_call_docs = _call.DocumentationLength()
-            call_docs = [
-                _call.Documentation(i).decode('utf8').strip()
-                for i in range(num_call_docs)
-            ]
+            call_attrs_dict = extract_attributes(_call)
 
             call_type = call_attrs_dict.get('type', None)
             if call_type not in INTERFACE_MEMBER_TYPES:
@@ -316,20 +393,13 @@ with open(filepath, 'rb') as f:
                 # 'offset': int(_call.Offset()),
             }
             #call['attrs'] = call_attrs_dict
-            call['docs'] = call_docs
+            call['docs'] = extract_docs(_call)
 
             calls.append(call)
             calls_by_name[_call_name] = call
 
         # service['calls'] = sorted(calls, key=lambda field: field['id'])
         service['slots'] = calls_by_name
-
-        num_service_docs = _service.DocumentationLength()
-        service_docs = [
-            _service.Documentation(i).decode('utf8').strip()
-            for i in range(num_service_docs)
-        ]
-        service['docs'] = service_docs
 
         services.append(service)
 
